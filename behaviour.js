44e1e16c37dc16f96b1b2f065af634edc5b8774a
prince-hubbard <princeperfect234@gmail.com>
2022-11-14 20:17:37 +0100

    second-commit

--- behaviour.js
+++ behaviour.js
@@ -1,9 +1,18 @@
-var links = document.getElementById("navLinks");
+// var links = document.getElementById("navLinks");
 
-function showMenu() {
-    links.style.right = "0";
-}
+// function showMenu() {
+//     links.style.right = "0";
+// }
 
-function hideMenu() {
-    links.style.right = "-200px";
+// function hideMenu() {
+//     links.style.right = "-200px";
+// }
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
